6d7e30334e9718ff58dfbd6f7949e17ef13d7425
Aidan Perret <aperret@me.com>
2021-06-18 13:04:49 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove()
+
+const newHeader = document.createElement('h1'); 
+newHeader.id = "victory";
+document.body.appendChild(newHeader); 
+newHeader.textContent = "Aidan is the champion"
